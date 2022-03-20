febe4e4146bf4924e12ec934201fffd6d09009b1
Ahmed-hussien90 <Ahmed.hs9090@gmail.com>
2022-03-20 16:45:37 +0200

    crop and rotate image

--- app/src/main/java/com/example/equationsolver/Home.java
+++ app/src/main/java/com/example/equationsolver/Home.java
@@ -6,21 +6,18 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -30,11 +27,11 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 import com.googlecode.tesseract.android.TessBaseAPI;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -42,12 +39,15 @@ public class Home extends AppCompatActivity {
 
     private ImageView galaryImage;
     private ImageView camImage;
+    private CropImageView cropImageView;
     private Button resButton;
+    private LinearLayout rotateLayout;
     private static final int REQUEST_GALLERY = 0;
     private static final int REQUEST_CAMERA = 1;
 
     private Uri imageUri;
     private TessBaseAPI baseAPI;
+
 
     private String resText = null;
 
@@ -61,6 +61,8 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        cropImageView = findViewById(R.id.cropImageView);
+        rotateLayout = findViewById(R.id.rotateLayout);
         drawerLayout = findViewById(R.id.drawerlayout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
@@ -69,7 +71,7 @@ public class Home extends AppCompatActivity {
         navigationView.getMenu().findItem(R.id.nav_history).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
-                startActivity(new Intent(getBaseContext(),History.class));
+                startActivity(new Intent(getBaseContext(), History.class));
                 return false;
             }
         });
@@ -77,11 +79,11 @@ public class Home extends AppCompatActivity {
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
 
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
         baseAPI = new TessBaseAPI();
+        baseAPI.setDebug(true);
+        baseAPI.setPageSegMode(TessBaseAPI.PageSegMode.PSM_SINGLE_LINE);
 
         File extDir = Environment.getExternalStorageDirectory();
         File appDir = new File(extDir, "TessOCR");
@@ -91,9 +93,6 @@ public class Home extends AppCompatActivity {
         if (!baseDir.isDirectory())
             baseDir.mkdir();
 
-
-        baseAPI.setDebug(true);
-        baseAPI.setPageSegMode(TessBaseAPI.OEM_CUBE_ONLY);
 
         try {
             String fileList[] = getAssets().list("");
@@ -156,11 +155,27 @@ public class Home extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-
+                inspectBMP(cropImageView.getCroppedImage());
                 Intent resIntent = new Intent(getApplicationContext(), ResultActivity.class);
                 if (resText != null)
                     resIntent.putExtra("res", resText);
                 startActivity(resIntent);
+
+            }
+        });
+
+        findViewById(R.id.rotate_left).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cropImageView.rotateImage(-90);
+
+            }
+        });
+
+        findViewById(R.id.rotate_right).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cropImageView.rotateImage(90);
 
             }
         });
@@ -174,155 +189,48 @@ public class Home extends AppCompatActivity {
         this.resText = text;
     }
 
-    private void inspect(Uri uri) {
-
-        InputStream is = null;
-        try {
-            is = getContentResolver().openInputStream(uri);
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-            options.inSampleSize = 4;
-            options.inScreenDensity = DisplayMetrics.DENSITY_LOW;
-            Bitmap bmp = BitmapFactory.decodeStream(is, null, options);
-
-            ImageView image = findViewById(R.id.editedImage);
-            image.setImageBitmap(bmp);
-            BitmapDrawable abmp = (BitmapDrawable) image.getDrawable();
-            bmp = abmp.getBitmap();
-            Bitmap temp = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight(), bmp.getConfig());
-
-            int srcData[] = new int[bmp.getWidth() * bmp.getHeight()];
-            int ptr = 0;
-            for (int i = 0; i < bmp.getWidth(); i++) {
-                for (int j = 0; j < bmp.getHeight(); j++) {
-                    int p = bmp.getPixel(i, j);
-                    int r = (int) (Color.red(p) * 0.33);
-                    int g = (int) (Color.green(p) * 0.59);
-                    int b = (int) (Color.blue(p) * 0.11);
-
-                    int av = (r + g + b) / 3;
-                    int n = r + g + b;
-                    r = g = b = n;
-
-                    srcData[ptr] = 0xFF & p;
-                    ptr++;
-
-                    temp.setPixel(i, j, Color.argb(Color.alpha(p), r, g, b));
-                }
-            }
-
-
-            int thresh = otsuThresholding(srcData);
-            double gamma_val = 1.25;
-            int[] gamma_lut = gamma_LUT(gamma_val);
-
-            Bitmap temp2 = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight(), bmp.getConfig());
-            for (int i = 0; i < temp.getWidth(); i++) {
-                for (int j = 0; j < temp.getHeight(); j++) {
-                    int p = temp.getPixel(i, j);
-
-                    int r = (gamma_lut[Color.red(p)]);
-                    int g = (gamma_lut[Color.green(p)]);
-                    int b = (gamma_lut[Color.blue(p)]);
-
-                    int n = (int) (r * 0.33 + g * 0.59 + b * 0.11);
-
-
-                    if (n < thresh)
-                        temp2.setPixel(i, j, Color.argb(Color.alpha(p), 255, 255, 255));
-                    else
-                        temp2.setPixel(i, j, Color.argb(Color.alpha(p), 0, 0, 0));
-                }
-            }
-
-            image.setImageBitmap(temp2);
-            inspectBMP(temp);
-            Toast.makeText(getApplicationContext(), "Threshlold=" + thresh, Toast.LENGTH_LONG).show();
-
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (IOException e) {
-                }
-            }
-        }
-    }
-
-    private int otsuThresholding(int[] src) {
-        int ptr = 0;
-        int hist[] = new int[256];
-        while (ptr < src.length) {
-            int h = src[ptr];
-            hist[h]++;
-            ptr++;
-        }
-        int total = src.length;
-        float sum = 0;
-        for (int t = 0; t < 256; t++) sum += t * hist[t];
-
-        float sumB = 0;
-        int wB = 0;
-        int wF = 0;
-
-        float varMax = 0;
-        int threshold = 0;
-
-        for (int t = 0; t < 256; t++) {
-            wB += hist[t];               // Weight Background
-            if (wB == 0) continue;
-
-            wF = total - wB;                 // Weight Foreground
-            if (wF == 0) break;
-
-            sumB += (float) (t * hist[t]);
-
-            float mB = sumB / wB;            // Mean Background
-            float mF = (sum - sumB) / wF;    // Mean Foreground
-
-            // Calculate Between Class Variance
-            float varBetween = (float) wB * (float) wF * (mB - mF) * (mB - mF);
-
-            // Check if new maximum found
-            if (varBetween > varMax) {
-                varMax = varBetween;
-                threshold = t;
-            }
-        }
-
-        return threshold;
-
-    }
-
-    private static int[] gamma_LUT(double gamma_new) {
-        int[] gamma_LUT = new int[256];
-
-        for (int i = 0; i < gamma_LUT.length; i++) {
-            gamma_LUT[i] = (int) (255 * (Math.pow((double) i / (double) 255, gamma_new)));
-        }
-
-        return gamma_LUT;
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // TODO Auto-generated method stub
 
         switch (requestCode) {
             case REQUEST_GALLERY:
 
                 if (resultCode == RESULT_OK) {
 
-                    inspect(data.getData());
+                    InputStream is = null;
+                    try {
+                        is = getContentResolver().openInputStream(data.getData());
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+                    options.inSampleSize = 4;
+                    options.inScreenDensity = DisplayMetrics.DENSITY_LOW;
+                    Bitmap bmp = BitmapFactory.decodeStream(is, null, options);
+                    cropImageView.setImageBitmap(bmp);
+                    rotateLayout.setVisibility(View.VISIBLE);
+
                 }
                 break;
             case REQUEST_CAMERA:
                 if (resultCode == RESULT_OK) {
                     if (imageUri != null) {
-                        inspect(imageUri);
+                        InputStream is = null;
+                        try {
+                            is = getContentResolver().openInputStream(imageUri);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                        BitmapFactory.Options options = new BitmapFactory.Options();
+                        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+                        options.inSampleSize = 4;
+                        options.inScreenDensity = DisplayMetrics.DENSITY_LOW;
+                        Bitmap bmp = BitmapFactory.decodeStream(is, null, options);
+                        cropImageView.setImageBitmap(bmp);
+                        rotateLayout.setVisibility(View.VISIBLE);
+
                     }
                 }
                 break;
